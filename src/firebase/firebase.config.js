e338cc35c25e76bb97378dc7b220282187568f29
devsrabon <et.srabon@gmail.com>
2022-11-08 19:26:24 +0600

    header added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBJGzl5LXbqIW83P_5VxU6_bVeKltPGid4',
+	authDomain: 'doc-service-949fd.firebaseapp.com',
+	projectId: 'doc-service-949fd',
+	storageBucket: 'doc-service-949fd.appspot.com',
+	messagingSenderId: '490273371293',
+	appId: '1:490273371293:web:9ed740f77ceca0d4869074',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import logo from '../../../assets/logo.svg';
+const Header = () => {
+    	const { user, logOut } = useContext(AuthContext);
+
+			const handleLogOut = () => {
+				logOut().then().catch();
+			};
+    const menuItems = (
+		<>
+			<li className="font-semibold">
+				<Link to="/">Home</Link>
+			</li>
+			<li className="font-semibold">
+				<Link to="/services">Services</Link>
+			</li>
+			<li className="font-semibold">
+				<Link to="/blog">Blog</Link>
+			</li>
+			{user?.email ? (
+				<>
+					<li className="font-semibold">
+						<button onClick={handleLogOut} className="btn-ghost">
+							Sign Out
+						</button>
+					</li>
+				</>
+			) : (
+				<li className="font-semibold">
+					<Link to="/login">Login</Link>
+				</li>
+			)}
+		</>
+	);
+
+    return (
+			<div className="navbar h-20 mb-12 pt-12 bg-base-100">
+				<div className="navbar-start">
+					<div className="dropdown">
+						
+						<ul
+							tabIndex={0}
+							className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+						>
+							{menuItems}
+						</ul>
+					</div>
+					<Link to="/" className="btn btn-ghost normal-case text-xl gap-2">
+                    <img src={logo} alt="" />
+                    <div className='mr-2'>Doctor Service</div>
+					</Link>
+				</div>
+				<div className="navbar-end hidden lg:flex">
+					<ul className="menu menu-horizontal p-0">{menuItems}</ul>
+				</div>
+				
+			</div>
+		);
+};
+
+export default Header;
