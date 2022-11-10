618c66e3a90c263f35c8511c9c4f611218408cdd
devsrabon <et.srabon@gmail.com>
2022-11-10 20:40:36 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyBJGzl5LXbqIW83P_5VxU6_bVeKltPGid4',
-	authDomain: 'doc-service-949fd.firebaseapp.com',
-	projectId: 'doc-service-949fd',
-	storageBucket: 'doc-service-949fd.appspot.com',
-	messagingSenderId: '490273371293',
-	appId: '1:490273371293:web:9ed740f77ceca0d4869074',
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
